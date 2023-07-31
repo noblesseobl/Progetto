cceae77325fb28711b5073e889e9df56071ea8ff
Antonio Giordano <antoniogiordano41@gmail.com>
2023-07-31 15:18:42 +0200

    aggiunto file

--- lib/transition.dart
+++ lib/transition.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 
-//aggiunto file transition
+//aggiunto file transitio
 class CustomPageRoute extends PageRouteBuilder{
   final Widget child;
   final AxisDirection direction;
